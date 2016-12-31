a64e5d553fc6dcbd546184304b598a4a6eb5efc3
shamtest <16955002+jackiegit@users.noreply.github.com>
2016-12-31 17:53:09 +0000

    facebook and game component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,20 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
-import { LoginComponent } from './facebook-login/login.component';
+import {FacebookService, FacebookLoginResponse, FacebookInitParams} from 'ng2-facebook-sdk';
+import { GameComponent } from './game/game.component';
 
 @Component({
   templateUrl: 'app.html',
+  providers: [FacebookService]
 })
-export class MyApp {
-  rootPage = LoginComponent;
+export class MyApp implements OnInit{
+  rootPage = GameComponent;
 
-  constructor(platform: Platform) {
+  constructor(private fb: FacebookService, platform: Platform) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
+    this.initFacebookApi();
   }
+
+  private initFacebookApi() {
+      let fbParams: FacebookInitParams = {
+                              appId: '1346620645394325',
+                              xfbml: true,
+                              version: 'v2.6'
+                              };
+      this.fb.init(fbParams);
+  }
+
+  private fbLogin() {
+    this.fb.login().then(
+    (response: FacebookLoginResponse) => console.log(response),
+    (error: any) => console.error(error));
+  }
+
+    ngOnInit() {
+      this.fb.getLoginStatus().then(fbResponse => {
+        let isConnected = fbResponse.status === "connected";
+        
+        console.log(isConnected);
+
+        if (!isConnected)
+          this.fbLogin();
+      });
+    }
 }
---
+++ client/src/app/game/game.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: '<game></game>',
+    templateUrl: 'game.html'
+})
+export class GameComponent {
+
+}
